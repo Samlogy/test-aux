87645473b25f461f272ecfe404a0a63db5406520
samlogy <aceportgasd235@gmail.com>
2024-01-20 14:05:34 +0100

    edit: catrd cpt

--- front/src/components/Card.tsx
+++ front/src/components/Card.tsx
@@ -1,18 +1,30 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, IconButton, Image } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import { CatModal } from "./";
+import { CatDelete, CatDetails } from "./";
 
+import { MdDelete, MdEdit } from "react-icons/md";
 import { ICat } from "../lib/interfaces";
 import { FavouriteButton } from "./";
 
-interface ICard {
-  cat: ICat;
-  readOnly?: boolean;
-}
-
-export default function Card({ cat, readOnly = false }: ICard) {
+export default function Card({ cat }: { cat: ICat }) {
   const [isOpen, setOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+
+  const onEdit = (e) => {
+    e.preventDefault();
+    setIsEdit(true);
+  };
+  const onDelete = (e) => {
+    e.preventDefault();
+    setIsDelete(true);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setOpen(!isOpen);
+  };
 
   return (
     <>
@@ -28,10 +40,7 @@ export default function Card({ cat, readOnly = false }: ICard) {
         initial={{ scale: 1 }}
         whileHover={{ scale: 1.02 }}
         whileTap={{ scale: 0.95 }}
-        onClick={(e) => {
-          e.preventDefault();
-          setOpen(!isOpen);
-        }}
+        onClick={handleClick}
       >
         <Box bg={"gray_9"} w="full" rounded="lg" shadow="lg" pos="relative">
           <FavouriteButton cat={cat} />
@@ -53,6 +62,25 @@ export default function Card({ cat, readOnly = false }: ICard) {
               >
                 {cat?.name}
               </Box>
+
+              <IconButton
+                aria-label="modifier-chat"
+                icon={<MdEdit size="18" />}
+                isRound
+                pos="absolute"
+                top="4"
+                right="4"
+                onClick={onEdit}
+              />
+              <IconButton
+                aria-label="supprimer-chat"
+                icon={<MdDelete size="18" />}
+                isRound
+                pos="absolute"
+                top="4"
+                right="16"
+                onClick={onDelete}
+              />
             </Flex>
 
             <Flex
@@ -64,7 +92,8 @@ export default function Card({ cat, readOnly = false }: ICard) {
         </Box>
       </Flex>
 
-      <CatModal cat={cat} isOpen={isOpen} setOpen={setOpen} />
+      <CatDetails cat={cat} isOpen={isOpen} setOpen={setOpen} isEdit={isEdit} />
+      <CatDelete catId={cat.id} isOpen={isDelete} setOpen={setIsDelete} />
     </>
   );
 }

39190afedb40596901d487e681a050fcfbefbe33
samlogy <aceportgasd235@gmail.com>
2024-01-20 21:28:30 +0100

    add: logo

---
+++ front/src/components/Logo.tsx
@@ -0,0 +1,15 @@
+import { Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function Logo({ size = "sm" }) {
+  const imageSize: any = {
+    sm: "3.5em",
+    md: "4.5em",
+    lg: "5.5em",
+  };
+  return (
+    <Link to="/cats">
+      <Image src="/logo.jpg" alt="logo" boxSize={imageSize[size]} />
+    </Link>
+  );
+}

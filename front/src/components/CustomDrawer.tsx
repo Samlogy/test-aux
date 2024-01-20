804bf9774699d4c1efc0e2be3d5b7c043934f0a7
samlogy <aceportgasd235@gmail.com>
2024-01-20 14:04:21 +0100

    add: custom side menu cpt

---
+++ front/src/components/CustomDrawer.tsx
@@ -0,0 +1,44 @@
+import {
+  // Drawer
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/react";
+
+interface ICustomDrawer {
+  isOpen: boolean;
+  onClose: any;
+  title?: string;
+  placement?: any;
+  size?: string;
+  body?: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+export default function CustomDrawer({
+  isOpen,
+  onClose,
+  title,
+  placement = "left",
+  size = "xs",
+  body,
+  footer,
+}: ICustomDrawer) {
+  return (
+    <Drawer size={size} isOpen={isOpen} placement={placement} onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        {title && <DrawerHeader>{title}</DrawerHeader>}
+        {body && <DrawerBody>{body}</DrawerBody>}
+        {footer && (
+          <DrawerFooter justifyContent="center">{footer}</DrawerFooter>
+        )}
+      </DrawerContent>
+    </Drawer>
+  );
+}
